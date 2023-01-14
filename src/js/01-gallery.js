6dca7a0587950901cc8a39d33164768509e2901b
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-01-14 14:53:05 +0200

    Add image gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    alt="${item.description}"
+                />
+            </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
