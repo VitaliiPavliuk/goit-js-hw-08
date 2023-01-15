fc78de8396d3d507fb2052fd6936fc050e82e339
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-01-15 18:01:21 +0200

    Add video current time + throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
 
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let time = 0;
+
+const setTime = () => {
+  try {
+    const timeFromLS = JSON.parse(
+      localStorage.getItem('videoplayer-current-time')
+    );
+
+    if (timeFromLS === null) {
+      return;
+    }
+
+    time = timeFromLS.seconds;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+setTime();
+
+player.setCurrentTime(time);
+
+const getTime = currentTime => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+};
+
+player.on('timeupdate', throttle(getTime, 1000));
