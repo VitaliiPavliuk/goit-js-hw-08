8a001cbfd5a07d2c2decb925124fc1308894434f
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-01-15 13:00:08 +0200

    Add JS for feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+var throttle = require('lodash.throttle');
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+const userData = {};
+
+const fillFeedbackForm = () => {
+  try {
+    const userDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    if (userDataFromLS === null) {
+      return;
+    }
+
+    for (const prop in userDataFromLS) {
+      feedbackForm.elements[prop].value = userDataFromLS[prop];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fillFeedbackForm();
+
+const onFeedbackFormInput = event => {
+  const { target } = event;
+
+  const name = target.name;
+  const value = target.value;
+
+  userData[name] = value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  console.log(userData);
+
+  feedbackForm.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackForm.addEventListener('input', throttle(onFeedbackFormInput, 500));
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
